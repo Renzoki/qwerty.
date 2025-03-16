9fc060e100d77053f315a576d781bd5dfcaf2b73
Renz <renzonifico@gmail.com>
2025-03-16 22:31:58 +0800

    NEW: function for adding eventListeners

--- Scripts/keydown.js
+++ Scripts/keydown.js
@@ -1,13 +1,45 @@
-const aKey = document.querySelector("#a")
+// const aKey = document.querySelector("#a")
 
-window.addEventListener("keydown", (e) => {
-    if (e.key === "a" || e.key === "A") {
-        aKey.classList.add("has-background-warning")
-    }
-})
+// window.addEventListener("keydown", (e) => {
+//     if (e.code === "KeyA") {
+//         aKey.classList.add("has-background-warning")
+//     }
+//     console.log(e.code)
+// })
 
-window.addEventListener("keyup", (e) => {
-    if (e.key === "a" || e.key === "A") {
-        aKey.classList.remove("has-background-warning")
+// window.addEventListener("keyup", (e) => {
+//     if (e.code === "KeyA") {
+//         aKey.classList.remove("has-background-warning")
+//     }
+// })
+
+const body = document.querySelector("body")
+class keyObject {
+    constructor(name, code) {
+        this.name = name
+        this.code = code
+        this.element = document.querySelector(`#${this.name}`);
     }
-})
+}
+
+function generateKeyListeners(objArr) {
+    objArr.forEach(obj => {
+        body.addEventListener("keydown", (e) => {
+            if (e.code === obj.code) {
+                obj.element.classList.add("has-background-warning")
+                console.log(e.code)
+            }
+        })
+
+        body.addEventListener("keyup", (e) => {
+            if (e.code === obj.code)
+                obj.element.classList.remove("has-background-warning")
+        })
+    });
+}
+
+const a = new keyObject("a", "KeyA")
+const b = new keyObject("b", "KeyB")
+const c = new keyObject("c", "KeyC")
+
+generateKeyListeners([a, b, c])

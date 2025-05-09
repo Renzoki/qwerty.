0b883ba007fb6e184d285e690a3611903c2c434c
Renz <renzonifico@gmail.com>
2025-05-09 14:26:27 +0800

    changed port name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"))
 app.set("view engine", "ejs")
